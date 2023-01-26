07e6f9f5dd80ba7ab7390c7193d8a7febcbcc9d1
Seto <setoseng@gmail.com>
2023-01-26 10:51:36 -0600

    Re-enable login and sign in

---
+++ src/pages/login.js
@@ -0,0 +1,17 @@
+import * as React from "react"
+
+import LogInForm from "../components/LogIn/LogInForm.component"
+import Seo from "../components/seo"
+
+import { LogInContainer } from "../styles/login.styles"
+
+
+const LogIn = () => (
+  // <LogInContainer>
+  //   <Seo title="Authentication" />
+  //   <LogInForm />
+  // </LogInContainer>
+  <></>
+)
+
+export default LogIn
