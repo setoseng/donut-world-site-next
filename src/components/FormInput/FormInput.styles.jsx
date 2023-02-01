606b10dabf149968e09eb56052a30ecd8b410a59
Seto <setoseng@gmail.com>
2023-02-01 11:42:23 -0600

    Minor styling fix

--- src/components/FormInput/FormInput.styles.jsx
+++ src/components/FormInput/FormInput.styles.jsx
@@ -1,16 +1,7 @@
-import styled, { css } from '@emotion/styled';
-
-const subColor = 'grey';
-const mainColor = 'black';
-
-const shrinkLabel = css`
-  top: -14px;
-  font-size: 12px;
-  color: ${mainColor};
-`
+import styled from '@emotion/styled';
 
 export const FormInputLabel = styled.label`
-  color: ${subColor};
+  color: black;
   font-size: 16px;
   font-weight: normal;
   position: absolute;
@@ -18,29 +9,25 @@ export const FormInputLabel = styled.label`
   left: 5px;
   top: 10px;
   transition: 300ms ease all;
-  ${({shrink}) => shrink && shrinkLabel};
 `
 
 export const Input = styled.input`
   background: none;
   background-color: white;
-  color: ${subColor};
+  color: black;
   font-size: 18px;
   padding: 10px 10px 10px 5px;
   display: block;
   width: 100%;
   border: none;
   border-radius: 0;
-  border-bottom: 1px solid ${subColor};
+  border-bottom: 1px solid black;
   margin: 25px 0;
 
   &:focus {
     outline: none;
   }
 
-  &:focus ~ ${FormInputLabel} {
-    ${shrinkLabel};
-  }
 `
 
 export const Group = styled.div`
