07f825c4247faa819612b675f283b7ed6a246658
Setya Seng <setoseng@gmail.com>
2023-02-20 22:45:37 -0600

    Minor App Bar Update

--- src/components/CustomAppBar/CustomAppBar.component.jsx
+++ src/components/CustomAppBar/CustomAppBar.component.jsx
@@ -7,7 +7,7 @@ import {
   AppBar,
   Toolbar,
   Box,
-  Typography,
+  Container,
   Link,
  } from "@mui/material"
 
@@ -32,38 +32,40 @@ const CustomAppBar = ({ children, props}) => {
         borderBottom: (t) => `1px solid ${t.palette.divider}`,
       }}
     >
-      <Toolbar>
-        <Box sx={{ flexGrow: 1 }}>
-          <Link
-            to="/"
-            component={GatsbyLink}
+      <Container>
+        <Toolbar>
+          <Box sx={{ flexGrow: 1 }}>
+            <Link
+              to="/"
+              component={GatsbyLink}
+              sx={{
+                textDecoration: 'none',
+                color: 'rgb(61 61 61 / 69%)',
+              }}
+            >
+              <StaticImage
+                width={47}
+                height={55}
+                src='../../images/logo-pink-svg.svg'
+                alt="Donut World Logo"
+                imgStyle={{objectFit:'fill'}}
+                quality={100}
+                placeholder='tracedSVG'
+            />
+            </Link>
+            
+          </Box>
+          <Box
             sx={{
-              textDecoration: 'none',
-              color: 'rgb(61 61 61 / 69%)',
+              display: 'flex',
+              flexDirection: 'row',
+              columnGap: 2,
             }}
           >
-            <StaticImage
-              width={47}
-              height={55}
-              src='../../images/logo-pink-svg.svg'
-              alt="Donut World Logo"
-              imgStyle={{objectFit:'fill'}}
-              quality={100}
-              placeholder='tracedSVG'
-          />
-          </Link>
-          
-        </Box>
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            columnGap: 2,
-          }}
-        >
-          {children}
-        </Box>
-      </Toolbar>
+            {children}
+          </Box>
+        </Toolbar>
+      </Container>
     </AppBar>
   )
 }
